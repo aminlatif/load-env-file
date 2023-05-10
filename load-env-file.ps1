524b591db05a4b4d73d9c536cf6e5b0ae8943909
Amin Latifkar <amin.latifkar@gmail.com>
2023-05-10 14:56:38 +0330

    corrected remote echo

--- load-env-file.ps1
+++ load-env-file.ps1
@@ -73,7 +73,7 @@ Function convertCommandsArrayToString{
 
   $commandsString = ""
   Foreach ($command in $commandsArray){
-    $commandsString += "echo `"remote:$ `" $command `n"
+    $commandsString += "echo 'remote:$ $command' `n"
     $commandsString += $command + "`n"
   }
 
